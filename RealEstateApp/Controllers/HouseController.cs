e82db49f1b5aa717c6ec4c1ab4a3f1eaf1943cf4
Brandon Hill <bhizz61@live.com>
2020-07-10 15:14:35 -0500

    Re-adding properties to House/List view

---
+++ RealEstateApp/Controllers/HouseController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateApp.Models;
+
+namespace RealEstateApp.Controllers
+{
+    public class HouseController : Controller
+    {
+        // GET: Default
+        IHouseRepository repository;
+        public HouseController (IHouseRepository repPassedIn)
+        {
+            repository = repPassedIn;
+        }
+        public ActionResult List()
+        {
+            return View(repository.Houses);
+        }
+        public ActionResult Edit(int houseId)
+        {
+            House house = repository.Houses.FirstOrDefault(p => p.HouseID == houseId);
+            ViewBag.RealtorID = new SelectList(repository.Realtors, "RealtorID",  "FirstName", house.RealtorID);
+            return View(house);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(House house)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveHouse(house);
+                TempData["message"] = string.Format("{0} has been saved", house.Street);
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return View(house);
+            }
+        }
+        public ActionResult Create()
+        {
+            ViewBag.REaltorID = new SelectList(repository.Realtors, "RealtorID", "FirstName");
+            return View("Edit", new House());
+        }
+        public ActionResult Delete(int houseId)
+        {
+            House deletedHouse = repository.DeleteHouse(houseId);
+            if(deletedHouse != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted", deletedHouse.Street);
+            }
+            return RedirectToAction("List");
+        }
+    }
+}
